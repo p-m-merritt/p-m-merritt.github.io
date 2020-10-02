8e4eb68f20849264addf1b61b5eb11e5dc706678
p-m-merritt <merritt.paige.m@gmail.com>
2020-10-02 20:53:46 +0000

    project update

--- ASD projects/walker/index.js
+++ ASD projects/walker/index.js
@@ -23,14 +23,15 @@ function runProgram(){
 
     var key = {
         "Left": 37,
-        "Up": 38,
+        "Up": 40,
         "Right": 39,
-        "Down": 40,
+        "Down": 38,
     }
 
   // one-time setup
     var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
     $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
+    $(document).on('keyup', handleKeyUp);
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -44,32 +45,15 @@ function runProgram(){
     function newFrame() {
         repositionGameItem();
         redrawGameItem();
+
+        handleKeyDown();
+        handleKeyUp();
     }
 
   /* 
   Called in response to events.
   */
 
-    function handleKeyDown() {
-        if (event.which === key.Left) {
-        speedX -= 5;
-        console.log("left pressed");
-        }
-        else if (event.which === key.Right) {
-        speedX += 5;
-        console.log("right pressed");
-        }
-
-        else if (event.which === key.Up) {
-        speedY += 5;
-        console.log("up pressed");
-        }
-        else if (event.which === key.Down) {
-        speedY -= 5;
-        console.log("down pressed");
-        }
-    }
-    
     
 
   ////////////////////////////////////////////////////////////////////////////////
@@ -87,8 +71,42 @@ function runProgram(){
             $("#gameItem").css("left", positionX);
             $("#gameItem").css("top", positionY);
         }
+
+    function handleKeyDown() {
+        if (event.which === key.Left) {
+        speedX -= 5;
+        console.log("left pressed by first player");
+        }
+        else if (event.which === key.Right) {
+        speedX += 5;
+        console.log("right pressed by first player");
+        }
+
+        else if (event.which === key.Up) {
+        speedY += 5;
+        console.log("up pressed by first player");
+        }
+        else if (event.which === key.Down) {
+        speedY -= 5;
+        console.log("down pressed by first player");
+        }
+    }
+    function handleKeyUp() {
+        if (event.which === key.Left) {
+        speedX = 0;
+        }
+        else if (event.which === key.Right) {
+        speedX = 0;
+        }
+
+        else if (event.which === key.Up) {
+        speedY = 0;
+        }
+        else if (event.which === key.Down) {
+        speedY = 0;
+        }
+    }
         
-  
     function endGame() {
         // stop the interval timer
             clearInterval(interval);
