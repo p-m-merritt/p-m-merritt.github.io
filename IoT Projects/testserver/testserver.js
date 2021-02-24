17d24e7273de3f381fe6f81032cd96896ab16b84
Paige Merritt <PAIGE@dionmerittsimac.attlocal.net>
2021-02-24 09:28:28 -0600

    2/24/21

--- IoT Projects/testserver/testserver.js
+++ IoT Projects/testserver/testserver.js
@@ -1,9 +1,11 @@
 var http = require("http");
-var port = 8686;
+var port = process.argv[2];
 
 http.createServer(function(req,res){
   // handle response
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello ');
-  res.end('World!');
+  res.write();
+  res.end();
 }).listen(port);
+
+console.log("Being server on port" + port);
